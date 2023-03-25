deb70834b250ece0bdabed6a8661b99498d8a9fc
t-benedek <t.benedek@web.de>
2023-03-25 17:38:22 +0100

    download and details seem to work now

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -4,42 +4,40 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.udacity.databinding.ActivityDetailBinding
+import com.udacity.databinding.ContentDetailBinding
 import com.udacity.utils.NameAndStatus
-import kotlinx.android.synthetic.main.activity_detail.*
-import kotlinx.android.synthetic.main.content_detail.view.*
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDetailBinding
+    private lateinit var binding: ContentDetailBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityDetailBinding.inflate(layoutInflater)
+        binding = ContentDetailBinding.inflate(layoutInflater)
 
         val nameAndStatus = intent.getParcelableExtra<NameAndStatus>("nameAndStatus")
 
-        binding.contentDetail.nameAndStatus = nameAndStatus
-//        setSupportActionBar(binding.toolbar)
-//        setContentView(binding.root)
-//
-//        //Setting color of the status
-//        if(nameAndStatus?.status  == "FAILED"){
-//            binding.contentDetail.statusTextValue.setTextColor(Color.RED)
-//        }
-//        else{
-//            binding.contentDetail.statusTextValue.setTextColor(Color.GREEN)
-//        }
-//
-//
-//        //Getting back to Main Activity Via an Intent
-//        binding.contentDetail.okButton.setOnClickListener{
-//            val intentToMain = Intent(this, MainActivity::class.java)
-//            startActivity(intentToMain)
-//            finish()
-//        }
+        binding.nameAndStatus = nameAndStatus
+        // setSupportActionBar(binding.toolbar)
+        setContentView(binding.root)
+
+        //Setting color of the status
+        if(nameAndStatus?.status  == "FAILED"){
+            binding.statusTextValue.setTextColor(Color.RED)
+        }
+        else{
+            binding.statusTextValue.setTextColor(Color.GREEN)
+        }
+
+
+        //Getting back to Main Activity Via an Intent
+        binding.okButton.setOnClickListener{
+            val intentToMain = Intent(this, MainActivity::class.java)
+            startActivity(intentToMain)
+            finish()
+        }
     }
 
 
